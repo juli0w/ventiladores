b4dcb14a4b2d19bfb5bdcc554187033d9300ef78
root <root@future.lojadopintor.net>
2015-06-25 22:42:55 -0300

    server

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://173.255.197.224:8080/admin/');
+define('HTTP_CATALOG', 'http://173.255.197.224:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://173.255.197.224:8080/admin/');
+define('HTTPS_CATALOG', 'http://173.255.197.224:8080/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/ventiladores/admin/');
+define('DIR_SYSTEM', '/var/www/ventiladores/system/');
+define('DIR_LANGUAGE', '/var/www/ventiladores/admin/language/');
+define('DIR_TEMPLATE', '/var/www/ventiladores/admin/view/template/');
+define('DIR_CONFIG', '/var/www/ventiladores/system/config/');
+define('DIR_IMAGE', '/var/www/ventiladores/image/');
+define('DIR_CACHE', '/var/www/ventiladores/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/ventiladores/system/download/');
+define('DIR_UPLOAD', '/var/www/ventiladores/system/upload/');
+define('DIR_LOGS', '/var/www/ventiladores/system/logs/');
+define('DIR_MODIFICATION', '/var/www/ventiladores/system/modification/');
+define('DIR_CATALOG', '/var/www/ventiladores/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ventiladores');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://173.255.197.224:8080/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://173.255.197.224:8080/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/ventiladores/catalog/');
+define('DIR_SYSTEM', '/var/www/ventiladores/system/');
+define('DIR_LANGUAGE', '/var/www/ventiladores/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/ventiladores/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/ventiladores/system/config/');
+define('DIR_IMAGE', '/var/www/ventiladores/image/');
+define('DIR_CACHE', '/var/www/ventiladores/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/ventiladores/system/download/');
+define('DIR_UPLOAD', '/var/www/ventiladores/system/upload/');
+define('DIR_MODIFICATION', '/var/www/ventiladores/system/modification/');
+define('DIR_LOGS', '/var/www/ventiladores/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ventiladores');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
